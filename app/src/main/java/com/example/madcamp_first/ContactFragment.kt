34a78dbfa17713bba8f87e4b50edea5041fc9ef9
seo3650 <seo3650@kaist.ac.kr>
2020-07-09 17:55:51 +0900

    Add basic contact info

--- app/src/main/java/com/example/madcamp_first/ContactFragment.kt
+++ app/src/main/java/com/example/madcamp_first/ContactFragment.kt
@@ -12,7 +12,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.madcamp_first.ui.main.PageViewModel
 import com.example.madcamp_first.ui.main.PlaceholderFragment
+import kotlinx.android.synthetic.main.activity_contact.*
+import kotlinx.android.synthetic.main.activity_contact.view.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 //import android.R
@@ -23,10 +26,56 @@ class ContactFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activity?.let {
-            val intent = Intent(context, ContactActivity::class.java)
+
+//        contactViewModel = ViewModelProviders.of(this).get(ContactViewModel::class.java).apply {
+//
+//        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.activity_contact, container, false)
+
+        val adapter = ContactAdapter({ contact ->
+            val intent = Intent(context, AddActivity::class.java)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_NAME, contact.name)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_NUMBER, contact.number)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_ID, contact.id)
+            startActivity(intent)
+        }, { contact ->
+            deleteDialog(contact)
+        })
+
+        val lm = LinearLayoutManager(context)
+        root.main_recycleview.adapter = adapter
+        root.main_recycleview.layoutManager = lm
+        root.main_recycleview.setHasFixedSize(true)
+
+        contactViewModel = ViewModelProviders.of(this).get(ContactViewModel::class.java)
+        contactViewModel.getAll().observe(this, Observer<List<Contact>> { contacts ->
+            adapter.setContacts(contacts!!)
+        })
+
+//        val button:  = root.findViewById(R.id.main_button)
+
+        root.main_button.setOnClickListener {
+            val intent = Intent(context, AddActivity::class.java)
             startActivity(intent)
         }
+
+        return root
+    }
+
+    private fun deleteDialog(contact: Contact) {
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage("Delete selected contact?")
+            .setNegativeButton("NO") { _, _ -> }
+            .setPositiveButton("YES") { _, _ ->
+                contactViewModel.delete(contact)
+            }
+        builder.show()
     }
 
     companion object {
