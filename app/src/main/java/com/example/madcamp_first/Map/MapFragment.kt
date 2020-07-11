4c3fc153cd17e9a40ff6c7ac9b0309d7c08bc8b8
Sunjuhyeong <munuwazzi@kaist.ac.kr>
2020-07-11 19:23:38 +0900

    Google Map API

--- app/src/main/java/com/example/madcamp_first/Map/MapFragment.kt
+++ app/src/main/java/com/example/madcamp_first/Map/MapFragment.kt
@@ -6,11 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.madcamp_first.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.OnMapReadyCallback
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory.newLatLng
 
-
-private const val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1
-
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapFragment: MapFragment
 
@@ -18,14 +23,23 @@ class MapFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    private lateinit var mMap: GoogleMap
 
-
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val marker = LatLng(35.241615, 128.695587)
+        mMap.addMarker(MarkerOptions().position(marker).title("Marker LAB"))
+        mMap.moveCamera(newLatLng(marker))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.activity_map, container, false)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
 
         return root
     }
--- app/src/main/java/com/example/madcamp_first/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/madcamp_first/ui/main/SectionsPagerAdapter.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.madcamp_first.Contact.ContactFragment
 import com.example.madcamp_first.Gallery.GalleryFragment
-
+import com.example.madcamp_first.Map.MapFragment
 import com.example.madcamp_first.R
 
 private val TAB_TITLES = arrayOf(
@@ -28,6 +28,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
         return when(position) {
             0 -> ContactFragment.newInstance()
             1 -> GalleryFragment.newInstance()
+            2 -> MapFragment.newInstance()
             else -> PlaceholderFragment.newInstance(position + 1)
         }
     }
